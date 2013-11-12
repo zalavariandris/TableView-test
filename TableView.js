acb2dd55337fe0f654024c2d3dca87b3d5a837b3
zalavariandris <zalavariandris@gmail.com>
2013-11-12 12:38:20 +0100

    first commit
    simple TableView Template to add/remove items from a specific
    collection (a test for heroku and mongolab)

---
+++ TableView.js
@@ -0,0 +1,18 @@
+collection = new Meteor.Collection('test-collection');
+if (Meteor.isClient) {
+
+  Template.body.collection = function() {
+    return collection.find( {} );
+  };
+  
+  Template.TableView.events({
+    'click [name=remove]': function(event, template) {
+      var cursor = template.data;
+      cursor.collection.remove( this );
+    },
+    'click [name=add]': function(event, template) {
+      var cursor = template.data;
+      cursor.collection.insert( {name: '-name-'} );
+    }
+  });
+}
